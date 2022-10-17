9f3d5966bd1966f1b56db45da2196d74e00490dd
Kendell Andrews <kendellandrews2@gmail.com>
2022-10-17 13:14:30 -0700

    end of monday

---
+++ movie-watchlist/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default Header
