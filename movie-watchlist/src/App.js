e7355f2012feb502cbad83a919282c20637a45d2
Kendell Andrews <kendellandrews2@gmail.com>
2022-10-24 10:11:29 -0700

    finished

--- movie-watchlist/src/App.js
+++ movie-watchlist/src/App.js
@@ -1,14 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/Header'
+import MovieScreen from './components/MovieScreen'
+import Watchlist from './components/Watchlist';
 
-function App() {
+function App () {
+  const [list, setList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1)
+
+  const addMovie = (movie) => setList([...list, movie]);
+
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setList(newState);
+  };
+  
+  const getData = () => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+    .then((res) => {
+      console.log(res.data.results);
+      setMovieList(res.data.results);
+    });
+  };
+  
+  useEffect(() => {
+    getData();
+  }, [page]);
+  
+  
   return (
     <div className="App">
       <Header/>
+        <main className='main'>
+          <MovieScreen className="main-movie"
+            list={list}
+            page={page}
+            setPage={setPage}
+            movieList={movieList}
+            addMovie={addMovie}
+            removeMovie={removeMovie}
+            />
+
+            <Watchlist className="main-watch"
+            list={list}
+            removeMovie={removeMovie}
+            />
+
+        </main>
     </div>
   );
+
 }
 
-export default Header
+export default App;
